915d78a839ac414f2aa337311b9c285f179ecd0e
Travis Blankenship <tblank555@gmail.com>
2014-04-14 10:17:59 -0700

    Added Appledoc-style comments to TABFileMutator.h

--- File Mutator/TABFileMutator.h
+++ File Mutator/TABFileMutator.h
@@ -23,7 +23,20 @@ NSURL *TABGenerateFile(NSURL *rootDirectory);
 
 @interface TABFileMutator : NSObject
 
+/**
+ Reads a file as a UTF-8 encoded string.
+ @param fileURL An `NSURL` object that points to the file to be read.
+ @param error A pointer to an `NSError` object that will contain an `NSError` in the event of an error.
+ @result An `NSString` containing the contents of the file pointed to by `fileURL`.
+ */
 + (NSString *)readFileAsUTF8String:(NSURL *)fileURL error:(NSError **)error;
+
+/**
+ Mutates a file by appending a random integer (uint32) to the end of the file.
+ @param fileURL An `NSURL` object that points to the file to be mutated.
+ @param error A pointer to an `NSError` object that will contain an `NSError` in the event of an error.
+ @result A Boolean value indicating whether or not the mutation operation was successful. If this value is `NO`, `error` will point to an `NSError` object describing the error.
+ */
 + (BOOL)mutateFile:(NSURL *)fileURL error:(NSError **)error;
 
 @end
